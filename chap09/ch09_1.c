7bb3fcf5427dfe5cadae6f5e8b37c56a6ab8a0dd
mirjam060518@gmail.com <mirjam060518@gmail.com>
2025-10-01 11:35:55 +0900

    2025.10.1

---
+++ chap09/ch09_1.c
@@ -0,0 +1,31 @@
+/*
+* 학번 : 202511216
+* 이름 : 나영탁
+* 프로그램 명 : 446p 예제 9-1.c
+* 날짜 : 2023.10.1
+*/
+
+#include <stdio.h>
+int exercise0901();
+
+
+int main(void)
+{
+	exercise0901();
+
+	return 0;
+}
+
+int exercise0901()
+{
+	char str1[10] = { 'a', 'b', 'c' };
+	char str2[10] = "abc";
+	char str3[] = "abc";					// str3은 크기가 4인 배열
+	char str4[10] = "very long string";		// 컴파일 경고
+	int size = sizeof(str1) / sizeof(str1[0]);
+	int i;
+
+	printf("str1 = ")
+	
+	return 0;
+}
